259b4ce0877de284e3923c2149769d061e922fce
laksix <147379934+laksix@users.noreply.github.com>
2024-03-20 20:28:32 +0700

    Update Slug.ts

--- src/components/models/Slug.ts
+++ src/components/models/Slug.ts
@@ -1,1 +1,41 @@
-export interface ISlugs {  articlesCount: number;  articles: ISlug[];}export interface ICurrentSlug {  article: ISlug;}export interface LikeSlug {  article: CurrentSlug;}export interface CurrentSlug {  slug: string;  title: string;  description: string;  body: string;  tagList: string[];  createdAt: string;  updatedAt: string;  favorited: boolean;  favoritesCount: number;  author: IAuthor;}export interface ISlug {  slug: string;  title: string;  description: string;  body: string;  tagList: string[];  createdAt: string;  updatedAt: string;  favorited: boolean;  favoritesCount: number;  author: IAuthor;}export interface IAuthor {  username: string;  bio: string;  image: string;  following: boolean;}
+export interface ISlugs {
+  articlesCount: number;
+  articles: ISlug[];
+}
+export interface ICurrentSlug {
+  article: ISlug;
+}
+export interface LikeSlug {
+  article: CurrentSlug;
+}
+
+export interface CurrentSlug {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  createdAt: string;
+  updatedAt: string;
+  favorited: boolean;
+  favoritesCount: number;
+  author: IAuthor;
+}
+export interface ISlug {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  createdAt: string;
+  updatedAt: string;
+  favorited: boolean;
+  favoritesCount: number;
+  author: IAuthor;
+}
+export interface IAuthor {
+  username: string;
+  bio: string;
+  image: string;
+  following: boolean;
+}
