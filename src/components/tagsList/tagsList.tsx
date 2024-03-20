fef4d2484d5977712b4b5e6b0324c4e6d70c70c8
laksix <147379934+laksix@users.noreply.github.com>
2024-03-20 20:29:41 +0700

    Update tagsList.tsx

--- src/components/tagsList/tagsList.tsx
+++ src/components/tagsList/tagsList.tsx
@@ -1,1 +1,16 @@
-import clasess from '../createNewUser/createNewUser.module.scss';const TagsList = ({ value, onDelete, onChange }) => {  return (    <div className={clasess.buttonsTags} id="currentTag">      <input type="text" value={value} onChange={onChange} maxLength={20} className={clasess.newTag} />      <div className={clasess.buttons}>        <div className={clasess.deleteButton} onClick={onDelete}>          Delete        </div>      </div>    </div>  );};export default TagsList;
+import clasess from '../createNewUser/createNewUser.module.scss';
+
+const TagsList = ({ value, onDelete, onChange }) => {
+  return (
+    <div className={clasess.buttonsTags} id="currentTag">
+      <input type="text" value={value} onChange={onChange} maxLength={20} className={clasess.newTag} />
+      <div className={clasess.buttons}>
+        <div className={clasess.deleteButton} onClick={onDelete}>
+          Delete
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TagsList;
