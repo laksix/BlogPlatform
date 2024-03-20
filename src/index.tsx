5f3c0af7722c8eb05a88671d3507ac3250a1b3d3
laksix <147379934+laksix@users.noreply.github.com>
2024-03-20 20:25:39 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,20 @@
-import React from 'react';import { createRoot } from 'react-dom/client';import './index.css';import App from './components/App/app';import { Provider } from 'react-redux';import { setupStore } from './components/store/store';import { BrowserRouter } from 'react-router-dom';/* eslint-disable  @typescript-eslint/no-non-null-assertion */const container = document.getElementById('root')!;const root = createRoot(container);const store = setupStore();root.render(  <BrowserRouter>    <Provider store={store}>      <App />    </Provider>  </BrowserRouter>);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/App/app';
+import { Provider } from 'react-redux';
+import { setupStore } from './components/store/store';
+import { BrowserRouter } from 'react-router-dom';
+/* eslint-disable  @typescript-eslint/no-non-null-assertion */
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+const store = setupStore();
+
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
