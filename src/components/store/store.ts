4556f4eac21d9edffb2992dc28c709f7db51f939
laksix <147379934+laksix@users.noreply.github.com>
2024-03-20 20:29:03 +0700

    Update store.ts

--- src/components/store/store.ts
+++ src/components/store/store.ts
@@ -1,1 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';import { combineReducers } from 'redux';import Aritcles from './reducers/AritcleSlice';import UserSlice from './reducers/UserSlice';import LikesArticle from './reducers/LikesArticle';const rootReducer = combineReducers({  Aritcles,  UserSlice,  LikesArticle,});export const setupStore = () => {  return configureStore({    reducer: rootReducer,  });};export type RootState = ReturnType<typeof rootReducer>;export type AppStore = ReturnType<typeof setupStore>;export type AppDispatch = AppStore['dispatch'];
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import Aritcles from './reducers/AritcleSlice';
+import UserSlice from './reducers/UserSlice';
+import LikesArticle from './reducers/LikesArticle';
+const rootReducer = combineReducers({
+  Aritcles,
+  UserSlice,
+  LikesArticle,
+});
+
+export const setupStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
