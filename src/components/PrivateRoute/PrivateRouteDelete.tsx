0a8843eb12f94d2d7ad0d67cb6ccc7956202322c
laksix <147379934+laksix@users.noreply.github.com>
2024-03-20 20:26:37 +0700

    Update PrivateRouteDelete.tsx

--- src/components/PrivateRoute/PrivateRouteDelete.tsx
+++ src/components/PrivateRoute/PrivateRouteDelete.tsx
@@ -1,1 +1,7 @@
-import { useAppSelector } from '../hooks/redux';import { Navigate, Outlet } from 'react-router-dom';const PrivateRouteDelete = () => {  const confirmDelete = useAppSelector((state) => state.Aritcles.statusDelete);  return !confirmDelete ? <Outlet /> : <Navigate to="/articles" />;};export default PrivateRouteDelete;
+import { useAppSelector } from '../hooks/redux';
+import { Navigate, Outlet } from 'react-router-dom';
+const PrivateRouteDelete = () => {
+  const confirmDelete = useAppSelector((state) => state.Aritcles.statusDelete);
+  return !confirmDelete ? <Outlet /> : <Navigate to="/articles" />;
+};
+export default PrivateRouteDelete;
