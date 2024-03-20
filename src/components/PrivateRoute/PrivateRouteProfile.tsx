16b63791882c1b47128deb5f88476c286616f3e3
laksix <147379934+laksix@users.noreply.github.com>
2024-03-20 20:26:29 +0700

    Update PrivateRouteProfile.tsx

--- src/components/PrivateRoute/PrivateRouteProfile.tsx
+++ src/components/PrivateRoute/PrivateRouteProfile.tsx
@@ -1,1 +1,10 @@
-import { useAppSelector } from '../hooks/redux';import { Navigate, Outlet } from 'react-router-dom';const PrivateRouteProfile = () => {  const isAuth = useAppSelector((state) => state.UserSlice.isAuth);  const completeEdit = useAppSelector((state) => state.Aritcles.createFinal);  const confirmDelete = useAppSelector((state) => state.Aritcles.statusDelete);  const editStatus = useAppSelector((state) => state.Aritcles.articleEditStatus);  return isAuth && !completeEdit && !confirmDelete && !editStatus ? <Outlet /> : <Navigate to="/articles" />;};export default PrivateRouteProfile;
+import { useAppSelector } from '../hooks/redux';
+import { Navigate, Outlet } from 'react-router-dom';
+const PrivateRouteProfile = () => {
+  const isAuth = useAppSelector((state) => state.UserSlice.isAuth);
+  const completeEdit = useAppSelector((state) => state.Aritcles.createFinal);
+  const confirmDelete = useAppSelector((state) => state.Aritcles.statusDelete);
+  const editStatus = useAppSelector((state) => state.Aritcles.articleEditStatus);
+  return isAuth && !completeEdit && !confirmDelete && !editStatus ? <Outlet /> : <Navigate to="/articles" />;
+};
+export default PrivateRouteProfile;
