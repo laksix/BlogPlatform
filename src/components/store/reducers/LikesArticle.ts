a66833ca921f31e97509a2a4ac74082f63a60041
laksix <147379934+laksix@users.noreply.github.com>
2024-03-20 20:29:25 +0700

    Update LikesArticle.ts

--- src/components/store/reducers/LikesArticle.ts
+++ src/components/store/reducers/LikesArticle.ts
@@ -1,1 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit';import { LikeSlug, CurrentSlug } from '../../models/Slug';import { PayloadAction } from '@reduxjs/toolkit';export interface IinitialState {  article: CurrentSlug;}const initialState: IinitialState = {  article: {    slug: '',    title: '',    description: '',    body: '',    tagList: [''],    createdAt: '',    updatedAt: '',    favorited: false,    favoritesCount: 0,    author: {      username: '',      bio: '',      image: '',      following: false,    },  },};export const LikesArticles = createSlice({  name: 'likesArticles',  initialState,  reducers: {    LikeCurrentArticle(state, action: PayloadAction<LikeSlug>) {      state.article = action.payload.article;    },    unLikeCurrentArticle(state, action: PayloadAction<LikeSlug>) {      state.article = action.payload.article;    },  },});export default LikesArticles.reducer;
+import { createSlice } from '@reduxjs/toolkit';
+import { LikeSlug, CurrentSlug } from '../../models/Slug';
+import { PayloadAction } from '@reduxjs/toolkit';
+
+export interface IinitialState {
+  article: CurrentSlug;
+}
+
+const initialState: IinitialState = {
+  article: {
+    slug: '',
+    title: '',
+    description: '',
+    body: '',
+    tagList: [''],
+    createdAt: '',
+    updatedAt: '',
+    favorited: false,
+    favoritesCount: 0,
+    author: {
+      username: '',
+      bio: '',
+      image: '',
+      following: false,
+    },
+  },
+};
+
+export const LikesArticles = createSlice({
+  name: 'likesArticles',
+  initialState,
+  reducers: {
+    LikeCurrentArticle(state, action: PayloadAction<LikeSlug>) {
+      state.article = action.payload.article;
+    },
+    unLikeCurrentArticle(state, action: PayloadAction<LikeSlug>) {
+      state.article = action.payload.article;
+    },
+  },
+});
+
+export default LikesArticles.reducer;
